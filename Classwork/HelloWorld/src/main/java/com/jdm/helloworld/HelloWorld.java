aac65a09d704aa42f673592282b8414dfa315599
Joe <56753837+Kizmit@users.noreply.github.com>
2021-07-26 13:25:15 -0500

    Created HelloWorld using NetBeans

---
+++ Classwork/HelloWorld/src/main/java/com/jdm/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @author Joe McAdams
+ * email  joedmcadams@gmail.com
+ * date   07/26/21
+ * purpose: My first java program using Apache Netbeans
+ */
+package com.jdm.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Joe!");
+    }
+}
