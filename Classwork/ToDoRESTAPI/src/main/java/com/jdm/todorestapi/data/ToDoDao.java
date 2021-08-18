ebaeb17f3def6bdf069f06518bbbe4aea7334e27
Joe <56753837+Kizmit@users.noreply.github.com>
2021-08-18 12:08:19 -0500

    SpringBoot/JDBC practice

---
+++ Classwork/ToDoRESTAPI/src/main/java/com/jdm/todorestapi/data/ToDoDao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jdm.todorestapi.data;
+
+import com.jdm.todorestapi.models.ToDo;
+import java.util.List;
+
+/**
+ *
+ * @author Joe
+ */
+public interface ToDoDao {
+
+    ToDo add(ToDo todo);
+
+    List<ToDo> getAll();
+
+    ToDo findById(int id);
+
+    // true if item exists and is updated
+    boolean update(ToDo todo);
+
+    // true if item exists and is deleted
+    boolean deleteById(int id);
+}
